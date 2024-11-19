fc292ca861ffae41d1e443c41d3462c938069e2e
buttheadgg <timofey11229@gmail.com>
2024-11-19 20:35:32 +0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Exchanger from './application/pages/Exchanger';
+import './App.module.scss';
+
+const App = () => {
+  return (
+    <Exchanger/>
+  );
+};
+
+export default App;
